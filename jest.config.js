2b857a5694f51a0955379fff31c966a8ff97c0cc
Danylo Pasko <danil.pasco@gmail.com>
2026-03-06 22:34:30 +0200

    add build directory

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
-};
-
